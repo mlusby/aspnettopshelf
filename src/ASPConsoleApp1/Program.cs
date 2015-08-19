2bd23b98db01816ef0ac97858fe40bd80a4576ec
Mark Lusby <mlusby@gmail.com>
2015-08-19 15:55:14 -0500

    initial commit

---
+++ src/ASPConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPConsoleApp1
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Hello from my first ASP.NET Console App!");
+        }
+    }
+}
